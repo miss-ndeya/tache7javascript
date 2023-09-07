8c13e0b8660ac86de96cc07f74d4e18a7984ed8a
unknown <miss-ndeya>
2023-09-07 12:08:41 +0200

    changement var par const

--- script.js
+++ script.js
@@ -2,25 +2,25 @@
 
 function afficherResultat() {
     // valeurs saisi par l'utilisateur
-    var nombre1 = parseFloat(document.getElementById("nombre1").value);
-    var nombre2 = parseFloat(document.getElementById("nombre2").value);
+    const nombre1 = parseFloat(document.getElementById("nombre1").value);
+    const nombre2 = parseFloat(document.getElementById("nombre2").value);
   
     // calcul 
-    var resultat = nombre1 * nombre2;
+    const resultat = nombre1 * nombre2;
   
     // Resultat
-    var Texte = nombre1 + " multiplié par " + nombre2 + " vaut " + resultat;
+    const Texte = nombre1 + " multiplié par " + nombre2 + " vaut " + resultat;
     document.getElementById("text").innerHTML = Texte;
 }
-var clikbouton = document.getElementById("Resultat");
+const clikbouton = document.getElementById("Resultat");
 clikbouton.addEventListener("click", afficherResultat);
 
 
             /***********Exercice2**********/ 
 
 // Demande à l'utilisateur d'entrer un nombre
-var n = parseInt(prompt("Veuillez entrer un nombre :"));
-var modulo = n % 4;
+const n = parseInt(prompt("Veuillez entrer un nombre :"));
+const modulo = n % 4;
 document.write(`Si l'on divise  ${n}  par 4, le reste est de ${modulo}`);
 
  
